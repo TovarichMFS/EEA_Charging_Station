e2072e791ccfd88d5f579a0adbcef63e6eb8487a
TovarichMFS <calmels.dimitri@gmail.com>
2018-06-02 18:38:12 +0200

    Updated sources

--- car.py
+++ car.py
@@ -2,21 +2,105 @@
 
 import asyncio
 import websockets
+import signal
 
-id = "ouiouoiuioui"
+batteryCapacity = 500
+quit = False
+
+def signal_handler(signal,frame):
+		global quit
+		quit = True
 
 async def plug():
-    async with websockets.connect('ws://localhost:8765') as websocket:
+	step = 0
+	battery = 123
+	money = 500.0
+	totalConsumed = 0
+	print(battery)
+	async with websockets.connect('ws://localhost:8765') as websocket:
+		while step!=-1:
+			if quit==False:
+				if step==0:
+					await websocket.send(f"Plugged")
+					print(f"> Plugged")
+					step += 1
+				elif step==1:
+					resp = await websocket.recv()
+					print(f"< {resp}")
+					if resp=='Connected':
+						await websocket.send(f"ID")
+						id = input("Enter your ID:")
+						await websocket.send(f"{id}")
+						print(f"> ID sent")
+						step += 1
+				elif step==2:
+					resp = await websocket.recv()
+					print(f"< {resp}")
 
-        await websocket.send('pluged')
-        print(f"> pluged")
+					if resp=='Authorized':
+						needed = batteryCapacity - battery
+						await websocket.send(f"Needs")
+						await websocket.send(f"{needed}")
+						print(f"> Charging request sent")
+						await websocket.send(f"{money}")
+						print(f"> Money balance sent")
 
-        resp = await websocket.recv()
-        print(f"< {resp}")
-        if resp=='Connected':
-        	await websocket.send(f"> {id}")
-        	print(f"> ID sent")
-        	resp = await websocket.recv()
-        	print(f"< {resp}")
+						resp = await websocket.recv()
+						print(f"< {resp} kWh allowed")
+						step += 1
+					else:
+						await websocket.send("Ended")
+						step = 5
+				elif step==3:
+					resp = await websocket.recv()
+					print(f"< {resp}")
+					if resp=='Charging begins':
+						resp = await websocket.recv()
+						while resp!='Complete' and quit==False:
+							print(f"> {resp} kWh consumed")
+							await websocket.send(f"Received")
+							battery += int(resp)
+							totalConsumed += int(resp)
+							resp = await websocket.recv()
+						step += 1
+				elif step==4:
+					bill = await websocket.recv()
+					print(f"> {bill} $ to pay")
+					money -= float(bill)
+					await websocket.send("Payed")
+					print(f"> Payed")
+					print("Balance :"+str(money)+" $")
+					resp = await websocket.recv()
+					print(f"> {resp}")
+					await websocket.send("Ended")
+					step += 1
+				elif step==5:
+					print("Unplug required")
+					step += 1
+				elif step==-1:
+					exit(0)
+			else:
+				if step!=6:
+					await websocket.send("Stop")
+					print('Stopping...')
+					print(f"> {resp} kWh consumed")
+					await websocket.send(f"Received")
+					battery += int(resp)
+					totalConsumed += int(resp)
+					print(totalConsumed)
+					resp = await websocket.recv()
+					print(f"> {resp}")
+					await websocket.send("Stop transaction")
+					bill = await websocket.recv()
+					print(f"> {bill} $ to pay")
+					money -= float(bill)
+					await websocket.send("Payed")
+					print(f"> Payed")
+					print("Balance :"+str(money)+" $")
+				else:
+					await websocket.send("Unplugged")
+				step=-1
+				print('> Unplugged')
 
+signal.signal(signal.SIGINT, signal_handler)
 asyncio.get_event_loop().run_until_complete(plug())
--- cs.py
+++ cs.py
@@ -4,21 +4,149 @@
 
 import asyncio
 import websockets
+import random
+import time
 
-async def handle(websocket, path):
+price = 0.2
+
+"""async def handle(websocket, path):
     call = await websocket.recv()
     print(f"< {call}")
 
     resp = f"Connected"
-
     await websocket.send(resp)
     print(f"> {resp}")
 
     call = await websocket.recv()
+    print(f"> ID: {call} received")
     if call!='':
 	    resp = f"Authorized"
 	    await websocket.send(resp)
 	    print(f"> {resp}")
+
+	    powerAmount = await websocket.recv()
+	    print(f"> {powerAmount} kWh needed")
+	    money = await websocket.recv()
+	    print(f"> {money} $ available")
+	    if float(money) - (float(powerAmount)*price) < 0:
+	    	powerAmount = float(money)/price
+
+	    resp = f"{powerAmount}"
+	    await websocket.send(resp)
+	    print(f"> {resp} kWh allowed")
+	    resp = f"Charging begins"
+	    await websocket.send(resp)
+	    print(f"> {resp}")
+	    total = 0
+
+	    while total<int(powerAmount):
+		    consumed = random.randint(1,80)
+		    if (total+consumed)>int(powerAmount):
+			    consumed = int(powerAmount) - total
+			    total = int(powerAmount)
+		    else:
+			    total += consumed
+		    await websocket.send(f"{consumed}")
+		    print(f"> {consumed} kWh consumed")
+		    time.sleep(1)
+
+	    await websocket.send("Complete")
+	    print(f"> Charging complete")
+	    bill = total*price
+	    resp = f"{bill}"
+	    await websocket.send(resp)
+	    print(f"> Billed {bill} $")
+	    call = await websocket.recv()
+	    if call=='Payed':
+		    print(f"> Received")
+		    resp = f"End"
+		    await websocket.send(resp)
+		    print(f"> Transaction ended")"""
+
+async def handle(websocket, path):
+    while True:
+	    call = await websocket.recv()
+	    if call=='Plugged':
+		    resp = f"Connected"
+		    await websocket.send(resp)
+		    print(f"> {resp}")
+	    elif call=='ID':
+		    call = await websocket.recv()
+		    print(f"> ID: {call} received")
+		    if call!='':
+			    resp = f"Authorized"
+			    await websocket.send(resp)
+			    print(f"> {resp}")
+		    else:
+			    resp = f"Unauthorized"
+			    await websocket.send(resp)
+			    print(f"> {resp}")
+	    elif call=='Needs':
+		    powerAmount = await websocket.recv()
+		    print(f"> {powerAmount} kWh needed")
+		    money = await websocket.recv()
+		    print(f"> {money} $ available")
+		    if float(money) - (float(powerAmount)*price) < 0:
+			    powerAmount = float(money)/price
+
+		    resp = f"{powerAmount}"
+		    await websocket.send(resp)
+		    print(f"> {resp} kWh allowed")
+		    resp = f"Charging begins"
+		    await websocket.send(resp)
+		    print(f"> {resp}")
+		    total = 0
+
+		    consumed = random.randint(1,80)
+		    await websocket.send(f"{consumed}")
+		    print(f"> {consumed} kWh consumed")
+		    total += consumed
+		    time.sleep(1)
+		    while total<int(powerAmount):
+			    call = await websocket.recv()
+			    print(f"< {call}")
+			    if call=='Received':
+				    consumed = random.randint(1,80)
+				    if (total+consumed)>int(powerAmount):
+					    consumed = int(powerAmount) - total
+					    total = int(powerAmount)
+				    else:
+					    total += consumed
+				    await websocket.send(f"{consumed}")
+				    print(f"> {consumed} kWh consumed")
+				    time.sleep(1)
+			    else:
+				    break
+
+		    if call!='Received':
+			    await websocket.send("Stopped")
+			    print(f"> Charging stopped")
+		    else:
+			    await websocket.send("Complete")
+			    print(f"> Charging complete")
+			    bill = total*price
+			    resp = f"{bill}"
+			    await websocket.send(resp)
+			    print(f"> Billed {bill} $")
+	    elif call=='Payed':
+		    print(f"< Received")
+		    resp = f"End"
+		    await websocket.send(resp)
+		    print(f"> Transaction ended")
+	    elif call=='Stop transaction':
+		    bill = total*price
+		    resp = f"{bill}"
+		    await websocket.send(resp)
+		    print(f"> Billed {bill} $")
+		    call = await websocket.recv()
+		    print(f"< {call}")
+		    print('< Unplugged')
+		    break
+	    elif call=='Ended':
+		    print("Waiting for unplug")
+	    elif call=='Unplugged':
+		    print('< Unplugged')
+		    break
 
 start_server = websockets.serve(handle, 'localhost', 8765)
 
